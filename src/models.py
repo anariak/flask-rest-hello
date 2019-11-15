e39f4614c467c5872f13d0fcc888bb49b10cabe3
francisco martinez <jua.martinez@outlook.es>
2019-11-15 10:56:11 -0300

    tareas testear

--- src/models.py
+++ src/models.py
@@ -23,3 +23,32 @@ class User(db.Model):
             "lastname": self.lastname,
             "rut": self.rut
         }
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    title = db.Column(db.String(25))
+    creation = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    date = db.Column(db.DateTime)
+    location = db.Column(db.String(50))
+    description = db.Column(db.String(150))
+    payment = db.Column(db.Float)
+  #  category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
+    status = db.Column(db.String(10))
+    # Outside Columns
+  #  category = db.relationship('Category', backref=db.backref('category', lazy=True))
+    user = db.relationship('User', backref=db.backref('user', lazy=True))
+    def __repr__(self):
+        return '<Task %r>' % self.title
+
+    def serialize(self):
+        return {
+            "user_id": self.user_id,
+            "title": self.title,
+            "category":self.category_id,
+            "creation": self.creation,
+            "date": self.date,
+            "localtion": self.location,
+            "description": self.description,
+            "payment": self.payment,
+            "status": self.status
+        }
